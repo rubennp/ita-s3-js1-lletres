ac31d7a008daf45b7aa737e2ea4728e9c8a802cc
Rubèn Nieto <ruben.np@me.com>
2021-05-16 10:21:53 +0200

    Fi ex3

--- codi.js
+++ codi.js
@@ -1,5 +1,34 @@
 // Exercici 1
 
-let nom = ['R', 'u', 'b', 'è', 'n', 'N', 'i','e','t','o','P','e','r','a','r','n','a','u'];
+// let nom = ['R', 'u', 'b', '3', 'n'];
+// let nom = ['A','l','e','j','a','n','d','r','o'];
+let nom = ['A','r','i','a','d','n','a'];
 
 for (let c in nom) console.log(nom[c]);
+
+// Exercici 2
+
+console.log("Partim del nom " + nom.join('').toUpperCase());
+
+for (let c in nom) {
+    let ch = nom[c].toUpperCase();
+    if (ch === 'A' || ch === 'E' || ch === 'I' || ch === 'O' || ch === 'U')
+        console.log("He trobat la VOCAL: " + ch);
+    else if (ch >= '0' && ch <= '9') 
+        console.log("Els noms de persones no contenen el número: " + ch);
+    else 
+        console.log("He trobat la CONSONANT: " +  ch);
+}
+
+// Exercici 3
+
+let mNom = new Map();
+
+for (let c in nom) {
+    let ch = nom[c].toUpperCase();
+    if (mNom.has(ch))
+        mNom.set(ch, mNom.get(ch) + 1);
+    else mNom.set(ch, 1);
+}
+
+console.log(mNom);
