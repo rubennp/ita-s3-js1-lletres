90d020f5b54892f78d9d82d53dacd782b07ffa2a
Rubèn Nieto <ruben.np@me.com>
2021-05-17 09:03:59 +0200

    Afegida Regex a ex2

--- codi.js
+++ codi.js
@@ -1,6 +1,6 @@
 // Exercici 1
 
-let nom = ['R', 'u', 'b', 'è', 'n'];
+let nom = ['R', 'u', 'b', 'e', 'n'];
 
 for (let c in nom) console.log(nom[c]);
 
@@ -10,7 +10,8 @@ console.log("Partim del nom " + nom.join('').toUpperCase());
 
 for (let c in nom) {
     let ch = nom[c].toUpperCase();
-    if (ch === 'A' || ch === 'E' || ch === 'I' || ch === 'O' || ch === 'U')
+    
+    if (/[AEIOU]/.test(ch))
         console.log("He trobat la VOCAL: " + ch);
     else if (ch >= '0' && ch <= '9') 
         console.log("Els noms de persones no contenen el número: " + ch);
